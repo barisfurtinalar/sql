13d64e6fcb21174d34560b4cfe5ddae9e2ea21e3
Baris Furtinalar <baris.dogan@gmail.com>
2023-08-24 16:18:22 +0200

    Add files via upload

---
+++ Get-DiskUtilisation.ps1
@@ -0,0 +1,25 @@
+$start = get-date
+$end = $start.addMinutes(0.5)
+$duration = new-timespan $start $end
+$count = 0
+$total = 0
+while($duration -gt 0) {
+$disks = Get-CimInstance -class Win32_PerfFormattedData_PerfDisk_LogicalDisk | Select-Object Name, DiskWritesPersec, DiskReadsPersec
+    foreach ($disk in $disks) {
+    $ReadsPerSec = $disk.DiskReadsPersec
+    $WritesPerSec = $disk.DiskWritesPersec
+
+      if ($disk.Name -ne '_Total' -and $disk.Name -ne 'C:' ) {
+         $count++
+         
+        Write-Host "$($disk.Name) Writes/s: $WritesPerSec === Reads/s: $ReadsPerSec "
+        Add-Content -Path C:\DiskUtilisationLogfile.txt -Value "$($disk.Name) Writes/s: $WritesPerSec === Reads/s: $ReadsPerSec <-- $(get-date)"
+        }
+    }
+
+    Start-Sleep -milliseconds 300
+    $duration = new-timespan $(Get-Date) $end
+}
+
+
+ 
