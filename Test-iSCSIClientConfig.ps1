5d4370c88c3048801f98380de44db3736857b794
Baris Furtinalar <baris.dogan@gmail.com>
2023-10-17 15:56:25 +0200

    Create Test-iSCSIClientConfig.ps1

---
+++ Test-iSCSIClientConfig.ps1
@@ -0,0 +1,19 @@
+$conn=Get-IscsiConnection | select ConnectionIdentifier, TargetAddress | Measure-Object
+$targetIP=Get-IscsiConnection | select TargetAddress 
+$mpioPolicy=Get-MSDSMGlobalDefaultLoadBalancePolicy
+
+$targetIP = Get-IscsiConnection | select -ExpandProperty TargetAddress | Sort-Object | Get-Unique -AsString
+
+if($conn.Count -gt 1){
+    Write-Output "###########"
+    Write-Output "OK - Number of iSCI sessions: $(($conn).Count)"
+}
+if((Get-IscsiTarget).IsConnected){
+    Write-Output "###########"
+    Write-Output "OK - iSCSI initiator is connected to target(s)"
+}
+if($targetIP.Count -gt 1){
+    Write-Output "###########"
+    Write-Output "OK - More than 2 iSCSI targets configured" 
+    Write-Output "###########"
+} 
