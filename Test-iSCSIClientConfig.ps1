0d675c1358175b1754c497e395a7fe0719d0df9d
Baris Furtinalar <baris.dogan@gmail.com>
2023-10-27 15:51:40 +0200

    Update Test-iSCSIClientConfig.ps1
    Added a new check to identify asymmetric iSCSI sessions (e.g odd number off sessions)

--- Test-iSCSIClientConfig.ps1
+++ Test-iSCSIClientConfig.ps1
@@ -1,6 +1,8 @@
 $conn=Get-IscsiConnection | select ConnectionIdentifier, TargetAddress | Measure-Object
 $targetIP=Get-IscsiConnection | select TargetAddress 
 $mpioPolicy=Get-MSDSMGlobalDefaultLoadBalancePolicy
+[double]$number=($conn.count)
+$even = $number % 2
 
 $targetIP = Get-IscsiConnection | select -ExpandProperty TargetAddress | Sort-Object | Get-Unique -AsString
 
@@ -17,6 +19,13 @@ if($targetIP.Count -gt 1){
     Write-Output "OK - More than 2 iSCSI targets configured" 
     Write-Output "###########"
 }
+if($even -eq 0){
+    Write-Output "###########"
+    Write-Output "OK - Even Number of iSCI sessions"
+}
+else{
+    Write-Output "###########"
+    Write-Output "Warning - Not Even Number of iSCI sessions"
+}
 Write-Output "MPIO policy set to: $mpioPolicy"
-Write-Output "###########" 
-
+Write-Output "###########"  
